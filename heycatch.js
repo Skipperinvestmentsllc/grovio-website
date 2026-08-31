cbb555ef4c07dd30f2d13e0365f79193192fd099
Sondra Skipper Kilian <skipperinvestmentsllc@gmail.com>
2026-08-30 21:47:12 -0400

    Add HeyCatch analytics

---
+++ heycatch.js
@@ -0,0 +1,9 @@
+import { analytics } from 'https://esm.sh/@heycatch/sdk@0.7.0';
+
+analytics.init({
+  projectKey: 'hck_pk_OvR29GcY3NH2fGnItR-ku5dawpUGJVv4',
+  install: {
+    framework: 'web',
+    agent: 'codex',
+  },
+});
